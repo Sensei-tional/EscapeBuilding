e15055509983b4f730d4a0b32a47e7e4bc09d02f
KBTM <kester.93@gmail.com>
2021-12-07 02:24:14 +0000

    Major Update
    - Added Editable door speeds
    - Added Delay for door close
    - Changed Framerate

--- Source/EscapeBuildling/OpenDoor.cpp
+++ Source/EscapeBuildling/OpenDoor.cpp
@@ -1,8 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "OpenDoor.h"
 #include "GameFramework/Actor.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
@@ -14,7 +15,6 @@ UOpenDoor::UOpenDoor()
 	// ...
 }
 
-
 // Called when the game starts
 void UOpenDoor::BeginPlay()
 {
@@ -23,16 +23,46 @@ void UOpenDoor::BeginPlay()
 	InitialYaw = GetOwner()->GetActorRotation().Yaw;
 	CurrentYaw = InitialYaw;
 	TargetYaw += InitialYaw;
-}
 
+	if (!PressurePlate)
+	{
+		UE_LOG(LogTemp, Error, TEXT("The Actor %s has OpenDoor but no pressure plate selected!"), *GetOwner()->GetName());
+	}
+
+	ActorThatOpen = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
 
 // Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	
+
+	if (PressurePlate && PressurePlate->IsOverlappingActor(ActorThatOpen))
+	{
+		OpenThatDoor(DeltaTime);
+		DoorLastOpened = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f"), DoorLastOpened);
+	}
+	else
+	{
+		if (GetWorld()->GetTimeSeconds() - DoorLastOpened > DoorDelay)
+		{
+			CloseThatDoor(DeltaTime);
+		}
+	}
+}
+
+void UOpenDoor::OpenThatDoor(float DeltaTime)
+{
 	FRotator OpenDoor(0.f, CurrentYaw, 0.f);
-	CurrentYaw = FMath::FInterpConstantTo(CurrentYaw, -TargetYaw, DeltaTime, 20);
+	CurrentYaw = FMath::Lerp(CurrentYaw, -TargetYaw, DeltaTime * DoorOpenSpeed);
 	GetOwner()->SetActorRotation(OpenDoor);
+}
+
+void UOpenDoor::CloseThatDoor(float DeltaTime)
+{
+	FRotator CloseDoor(0.f, CurrentYaw, 0.f);
+	CurrentYaw = FMath::Lerp(CurrentYaw, InitialYaw, DeltaTime * DoorCloseSpeed);
+	GetOwner()->SetActorRotation(CloseDoor);
 }
 
--- Source/EscapeBuildling/OpenDoor.h
+++ Source/EscapeBuildling/OpenDoor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
@@ -23,13 +24,28 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	void OpenThatDoor(float DeltaTime);
+	void CloseThatDoor(float DeltaTime);
+
 
 private:
 
 	float InitialYaw, CurrentYaw;
+	float DoorLastOpened = 0.f;
+	float DoorDelay = 0.25f;
 
 	UPROPERTY(EditAnywhere)
-	
 		float TargetYaw = 90.f;
-		
+	
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume *PressurePlate;
+
+	UPROPERTY(EditAnywhere)
+		AActor* ActorThatOpen;
+
+	UPROPERTY(EditAnywhere)
+		float DoorOpenSpeed = 1.f;
+
+	UPROPERTY(EditAnywhere)
+		float DoorCloseSpeed = 2.f;
 };
