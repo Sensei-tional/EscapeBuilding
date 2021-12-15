2ecd2e25297b5bb989d6da31ad26471bfe9cbe04
KBTM <kester.93@gmail.com>
2021-12-15 03:24:05 +0000

    Grabber updated
    Had to rollback changes to be able to fix crash. Source control is tuff
    (┬┬﹏┬┬)
    
    - Added grabber on default pawn
    - Added Location and Rotation to log

---
+++ Source/EscapeBuildling/Grabber.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Grabber.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Get Player Viewpoint
+	FVector PlayerLocation = GetOwner()->GetActorLocation();
+	FRotator PlayerRotation = GetOwner()->GetActorRotation();
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerLocation, OUT PlayerRotation);
+	
+	UE_LOG(LogTemp, Warning, TEXT("The %s is located at: %s , rotation is: %s"),*GetOwner()->GetName(), *PlayerLocation.ToString(), *PlayerRotation.ToString());
+}
+
